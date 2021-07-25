addef28e07d15173fb7476b39e706da2a1aebf71
jacekpoz <jacek@techkom.pl>
2021-07-25 11:39:17 +0200

    added readme and for some reason didn't include MessageScreen in the last commit so here we go

---
+++ README.md
@@ -0,0 +1,17 @@
+# jacekpoz-chat
+yeah, the name is fucking horrendous, but I haven't thought of a better one, so I'll change it later
+
+the gui and design and shit so far cause I don't have much experience in swing, so I just tried to replicate messenger's design
+
+my friend has an idea for the looks though, so I'll work on that later
+
+the app overall is very bare bones, you can only add friends, create chats and send messages to those chats but ofc I'll add more things later on
+
+I'll write down the roadmap here so I won't forget
+
+- massive redesign
+- add profile pictures
+- overall add sending images and support for opening shit like images and videos in the app
+- add markdown support maybe idk
+- make an android client (haven't ever done anything in android so this might take a while)
+- uhhhhhh ummmmmmmmmm
--- src/main/java/com/github/jacekpoz/client/gui/screens/MessageScreen.java
+++ src/main/java/com/github/jacekpoz/client/gui/screens/MessageScreen.java
@@ -1,66 +1,45 @@
 package com.github.jacekpoz.client.gui.screens;
 
-import com.github.jacekpoz.client.gui.ChatContainer;
-import com.github.jacekpoz.client.gui.ChatPanel;
-import com.github.jacekpoz.client.gui.ChatWindow;
-import com.github.jacekpoz.client.gui.MessageContainer;
+import com.github.jacekpoz.client.gui.*;
 import com.github.jacekpoz.common.*;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.EOFException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Random;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MessageScreen extends JPanel {
-
-    private ChatWindow window;
+public class MessageScreen implements Screen {
+    private final ChatWindow window;
     private DatabaseConnector connector;
+
+    private JPanel messageScreen;
+    private JButton chatsButton;
+    private JTextField messageField;
+    private JButton sendMessageButton;
+    private JButton friendsButton;
+    private MessageContainer messages;
+    private JScrollPane messagesScrollPane;
+    private ChatContainer chats;
+    private JScrollPane chatsScrollPane;
 
     public MessageScreen(ChatWindow w) {
         window = w;
         try {
             connector = new DatabaseConnector(
-                    "jdbc:mysql://localhost:3306/" + GlobalStuff.DB_NAME,
+                    "jdbc:mysql://localhost:3306/" + Constants.DB_NAME,
                     "chat-client", "DB_Password_0123456789"
             );
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        initUI();
-    }
 
-    private void initUI() {
-
-        JButton friends = new JButton(
-                new ImageIcon(
-                        new ImageIcon("src/main/resources/friends.png")
-                                .getImage()
-                                .getScaledInstance(25, 25, Image.SCALE_SMOOTH)
-                ));
-        friends.addActionListener(a -> window.setScreen(window.getFriendsScreen()));
-
-        ChatContainer chats = new ChatContainer();
-
-        JScrollPane chatsScrollPane = new JScrollPane(chats);
-        chatsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        chats.setPreferredSize(new Dimension(100, 250));
-
-        MessageContainer messages = new MessageContainer();
-
-        JScrollPane messagesScrollPane = new JScrollPane(messages);
-        messagesScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        messagesScrollPane.setPreferredSize(new Dimension(500, 250));
-
-        JTextField messageField = new JTextField();
-
-        ActionListener sendMessageAction = event -> {
+        ActionListener sendMessageAction = e -> {
             if (!messageField.getText().isEmpty()) {
                 Chat c = window.getClient().getChat();
                 Message m = new Message(
@@ -70,7 +49,7 @@ public class MessageScreen extends JPanel {
                         messageField.getText(),
                         Timestamp.valueOf(LocalDateTime.now())
                 );
-                messages.addMessage(m);
+                c.getMessageIDs().add(m.getMessageID());
                 connector.addMessage(m);
                 sendMessage(m);
                 messageField.setText("");
@@ -80,11 +59,9 @@ public class MessageScreen extends JPanel {
         };
 
         messageField.addActionListener(sendMessageAction);
-
-        JButton button = new JButton("Wyślij");
-        button.addActionListener(sendMessageAction);
-
-        createLayout(friends, chatsScrollPane, messagesScrollPane, messageField, button);
+        sendMessageButton.addActionListener(sendMessageAction);
+        friendsButton.addActionListener(e -> window.setScreen(window.getFriendsScreen()));
+        chatsButton.addActionListener(e -> window.setScreen(window.getCreateChatsScreen()));
 
         ExecutorService service = Executors.newCachedThreadPool();
 
@@ -93,7 +70,9 @@ public class MessageScreen extends JPanel {
                 try {
                     Message fromServer = (Message) window.getInputStream().readObject();
 
-                    SwingUtilities.invokeLater(() -> messages.addMessage(fromServer));
+                    SwingUtilities.invokeLater(() -> messages.addMessage(
+                            new MessagePanel(connector.getUser(fromServer.getAuthorID()), fromServer)
+                    ));
 
                 } catch (EOFException e) {
                     System.err.println("EOF");
@@ -106,56 +85,46 @@ public class MessageScreen extends JPanel {
         });
     }
 
-    private void createLayout(JComponent... args) {
-        GroupLayout gl = new GroupLayout(this);
-        setLayout(gl);
-
-        gl.setAutoCreateGaps(true);
-        gl.setAutoCreateContainerGaps(true);
-
-        gl.setHorizontalGroup(
-                gl.createSequentialGroup()
-                        .addGroup(
-                                gl.createParallelGroup()
-                                        .addComponent(args[0])
-                                        .addComponent(args[1])
-                        )
-                        .addGroup(
-                                gl.createParallelGroup()
-                                        .addComponent(args[2])
-                                        .addGroup(
-                                                gl.createSequentialGroup()
-                                                        .addComponent(args[3])
-                                                        .addComponent(args[4])
-                                        )
-                        )
-        );
-
-        gl.setVerticalGroup(
-                gl.createParallelGroup()
-                        .addGroup(
-                                gl.createSequentialGroup()
-                                        .addComponent(args[0])
-                                        .addComponent(args[1])
-                        )
-                        .addGroup(
-                                gl.createSequentialGroup()
-                                        .addComponent(args[2])
-                                        .addGroup(
-                                                gl.createParallelGroup()
-                                                        .addComponent(args[3])
-                                                        .addComponent(args[4])
-                                        )
-                        )
-        );
-    }
-
     private void sendMessage(Message message) {
         try {
             System.out.println("sendMessage: " + message);
             window.getOutputStream().writeObject(message);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void setChat(Chat c) {
+        try {
+            window.getClient().setChat(c);
+            window.getOutputStream().writeObject(c);
+            messages.removeAllMessages();
+            c.getMessageIDs().forEach(mID -> {
+                Message m = connector.getMessage(mID, c.getId());
+                User author = connector.getUser(m.getAuthorID());
+                messages.addMessage(new MessagePanel(author, m));
+            });
+            messageField.setEnabled(true);
+            sendMessageButton.setEnabled(true);
+            messages.revalidate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public JPanel getPanel() {
+        return messageScreen;
+    }
+
+    @Override
+    public void update() {
+        chats.removeAllChats();
+        for (Chat c : connector.getUsersChats(window.getClient().getUser().getId())) {
+            ChatPanel cp = new ChatPanel(this, chats, c);
+            cp.setToolTipText(Util.userListToString(c.getMembers()));
+            chats.addChat(cp);
+            System.out.println("added chat: " + c);
         }
     }
 }
